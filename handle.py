9e634210d10f7a9d6684d1386eae7a937b61d2bd
kanshan <zots0127@gmail.com>
2021-01-20 03:15:23 +0800

    Update handle.py
    check token

--- handle.py
+++ handle.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-#
 # filename: handle.py
-
 import hashlib
+import reply
+import receive
 import web
-
 class Handle(object):
     def GET(self):
         try:
@@ -28,3 +28,20 @@ class Handle(object):
                 return ""
         except Exception, Argument:
             return Argument
+    def POST(self):
+        try:
+            webData = web.data()
+            print "Handle Post webdata is ", webData
+            #后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                content = "test"
+                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                return replyMsg.send()
+            else:
+                print "wait to process"
+                return "success"
+        except Exception, Argment:
+            return Argment
